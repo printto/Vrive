49a525945660ed52e618498c30c91776cca2d509
Pappim Pipatkasrira <printtopipatkasira@gmail.com>
2019-08-08 12:30:39 +0300

    Added digital speedometer

---
+++ Vrive/Assets/Scripts/SpeedTracker.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpeedTracker : MonoBehaviour
+{
+
+    public Text speedText;
+    public GameObject objectToTrack;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(speedText != null && objectToTrack != null)
+        {
+            speedText.text = ((int)objectToTrack.GetComponent<Rigidbody>().velocity.magnitude) + " KPH";
+        }
+    }
+}
