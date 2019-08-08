b427b18bfa9d6404dad552526f635a0c2c8d32cc
Pipatkasrira Pappim TIX18 <pappimpipatkasrira@kamk.fi>
2019-08-08 14:46:11 +0300

    vr textmeshpro ui

--- Vrive/Assets/Scripts/Timer.cs
+++ Vrive/Assets/Scripts/Timer.cs
@@ -1,13 +1,14 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class Timer : MonoBehaviour {
 
-    public Text timerText;
+    public GameObject timerText;
     private float startTime;
 
     private void Start()
@@ -22,9 +23,9 @@ public class Timer : MonoBehaviour {
         string minutes = ((int) t / 60).ToString();
         string seconds = (t % 60).ToString("f2");
 
-        timerText.text = minutes + ":" + seconds;
-
-        CurrentTime.GetCurrentTimeString = timerText.text;
+        //timerText.text = minutes + ":" + seconds;
+        timerText.GetComponent<TextMeshPro>().SetText(minutes + ":" + seconds);
+        CurrentTime.GetCurrentTimeString = timerText.GetComponent<TextMeshPro>().text;
 
     }
 }
