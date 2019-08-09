c75082b4ab1753827ed447440a8e0365d91efa16
Theeruth Borisuth <theeruth.b@ku.th>
2019-08-09 13:48:45 +0300

    add minimap

---
+++ Vrive/Assets/disableShadow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class disableShadow : MonoBehaviour
+{
+    private float previousShadowDistance;
+    void OnPreRender()
+    {
+        previousShadowDistance = QualitySettings.shadowDistance;
+        QualitySettings.shadowDistance = 0;
+    }
+    void OnPostRender()
+    {
+        QualitySettings.shadowDistance = previousShadowDistance;
+    }
+
+}
