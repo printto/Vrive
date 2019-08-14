5706c479597d2c7120352e1ce0c31501e873dd8d
Thongbor Thitiwat TIX18 <thitiwatthongbor@kamk.fi>
2019-08-14 13:27:13 +0300

    updatE

--- Vrive/Assets/Scripts/VRController/DDebug.cs
+++ Vrive/Assets/Scripts/VRController/DDebug.cs
@@ -1,12 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class DDebug : MonoBehaviour
 {
     [SerializeField]
-    Text DebugText;
+    TextMeshPro DebugText;
 
     public static DDebug Instance;
 
