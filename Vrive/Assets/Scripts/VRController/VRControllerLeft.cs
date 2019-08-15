345441466720ab3d6118e473116d5320166364e6
ZivesO <thitiwat.thongbor@gmail.com>
2019-08-15 17:09:13 +0300

    tested with controller

--- Vrive/Assets/Scripts/VRController/VRControllerLeft.cs
+++ Vrive/Assets/Scripts/VRController/VRControllerLeft.cs
@@ -5,31 +5,5 @@ using Valve.VR;
 
 public class VRControllerLeft : Abstract_VRController
 {
-    public override void BackingDown(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
-    {
-        DDebug.Instance.Log("Backing down");
-        Debug.Log("Backing down");
-        isBacking = true;
-    }
-
-    public override void BackingUp(SteamVR_Action_Boolean fromActive, SteamVR_Input_Sources fromSource)
-    {
-        DDebug.Instance.Log("Backing Up");
-        Debug.Log("Backing Up");
-        isBacking = false;
-    }
-
-    public override void BreakingDown(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
-    {
-        DDebug.Instance.Log("Breaking down");
-        Debug.Log("Breaking down");
-        isBreaking = true;
-    }
-
-    public override void BreakingUp(SteamVR_Action_Boolean fromActive, SteamVR_Input_Sources fromSource)
-    {
-        DDebug.Instance.Log("Breaking Up");
-        Debug.Log("Breaking Up");
-        isBreaking = false;
-    }
+    
 }
--- Vrive/Assets/Scripts/VRController/VRControllerRight.cs
+++ Vrive/Assets/Scripts/VRController/VRControllerRight.cs
@@ -19,4 +19,32 @@ public class VRControllerRight : Abstract_VRController
         Debug.Log("Forwarding Up");
         isForwarding = false;
     }
+
+    public override void BackingDown(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
+    {
+        DDebug.Instance.Log("Backing down");
+        Debug.Log("Backing down");
+        isBacking = true;
+    }
+
+    public override void BackingUp(SteamVR_Action_Boolean fromActive, SteamVR_Input_Sources fromSource)
+    {
+        DDebug.Instance.Log("Backing Up");
+        Debug.Log("Backing Up");
+        isBacking = false;
+    }
+
+    public override void BreakingDown(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
+    {
+        DDebug.Instance.Log("Breaking down");
+        Debug.Log("Breaking down");
+        isBreaking = true;
+    }
+
+    public override void BreakingUp(SteamVR_Action_Boolean fromActive, SteamVR_Input_Sources fromSource)
+    {
+        DDebug.Instance.Log("Breaking Up");
+        Debug.Log("Breaking Up");
+        isBreaking = false;
+    }
 }
