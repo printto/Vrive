53e7cd097d211b6b37365e703b1496fc76c5e4b6
Theeruth Borisuth <theeruth.b@ku.th>
2019-08-19 12:48:29 +0300

    add menu and retry button?

---
+++ Vrive/Assets/startGame.cs
@@ -0,0 +1,15 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class startGame : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.Equals("hand"))
+        {
+            SceneManager.LoadScene("Track");
+        }
+    }
+}
